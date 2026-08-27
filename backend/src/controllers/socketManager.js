317e2d1ce827719874dfacc6daf62da27b9e1bc1
jayesh-132005-gif <dev.jayeshsingh@gmail.com>
2026-08-27 20:22:17 +0530

    Add code of chat-message event

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -5,13 +5,13 @@ let messages = {};
 let timeOnLine = {};
 
 export const connectToSocket = (server) => {
-    // We create a socket server
+    // We create a socket server.
     const io = new Server(server);
 
-    // Socket server connection
+    // Socket server connection.
     io.on("connection", (socket) => {
         
-        // Multiple handling of events
+        // Multiple handling of events.
         socket.on("join_call", (path) => {
             
             // Adding user to room
@@ -23,7 +23,7 @@ export const connectToSocket = (server) => {
 
             timeOnLine[socket.id] = new Date();
 
-            // Sending new users to old users
+            // Sending new users to old users.
             for (let a = 0; a < connections[path].length; a++) {
                 io.to(connections[path][a]).emit(
                     "user_joined", 
@@ -31,7 +31,7 @@ export const connectToSocket = (server) => {
                     connections[path]);
             }
             
-            // Sending old chat of room to new users
+            // Sending old chat of room to new users.
             if (messages[path] !== undefined) {
                 for (let a = 0; a < messages[path].length; a++) {
                     io.to(socket.id).emit(
@@ -45,14 +45,46 @@ export const connectToSocket = (server) => {
         });
 
     });
-
+        
+        // To send a messgae from (socket.id)sender to (toId)receiver. 
         socket.on("signal", (toId, message) => {
-
+            io.to(toId).emit("signal", socket.id, message);
         });
 
-
+        // Have this Functionalities
+        // Receive message from client → Identify a room of client → Store a message in the history → Share a message to all clients.
         socket.on("chat_message", (data, sender) => {
 
+            const [ matchingRoom, found ] = Object.entries(connections)
+                .reduce(([room, isFound], [roomKey, roomValue]) => {
+
+                    if(!isFound && roomValue.includes(socket.id)) {
+                        return [roomKey, true];
+                    }
+
+                    return [room, isFound];
+
+                }, ['', false]);
+
+            if (found === true) {
+
+                if(messages[matchingRoom] === undefined) {
+                    messages[matchingRoom] = [];
+                }
+
+                messages[matchingRoom].push({
+                    'data': data,
+                    'sender': sender,
+                    'socket-id-sender': socket.id
+                })
+
+                console.log("messages", key, ":", sender, data)
+
+                connections[matchingRoom].forEach((elem) => {
+                    io.to(elem).emit("chat_message", data, sender, socket.id);
+                });
+
+            }    
 
         });
 
