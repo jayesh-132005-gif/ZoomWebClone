60bddd260d11c66b920e9c65772184b5382893e2
jayesh-132005-gif <dev.jayeshsingh@gmail.com>
2026-09-01 20:05:13 +0530

    write code of disconnect event

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -1,9 +1,14 @@
 import { Server } from "socket.io";
 
+// TO save a connection information with room name and socket id
 let connections = {};
+// To save a message information with room name and message data
 let messages = {};
+// To save a time information with socket id and time of connection
 let timeOnLine = {};
 
+
+// Socket server initialization function.
 export const connectToSocket = (server) => {
     // We create a socket server.
     const io = new Server(server);
@@ -18,9 +23,7 @@ export const connectToSocket = (server) => {
             if(connections[path] === undefined) {
                 connections[path] = [];
             }
-
             connections[path].push(socket.id);
-
             timeOnLine[socket.id] = new Date();
 
             // Sending new users to old users.
@@ -43,9 +46,7 @@ export const connectToSocket = (server) => {
             }
 
         });
-
-    });
-        
+  
         // To send a messgae from (socket.id)sender to (toId)receiver. 
         socket.on("signal", (toId, message) => {
             io.to(toId).emit("signal", socket.id, message);
@@ -57,43 +58,52 @@ export const connectToSocket = (server) => {
 
             const [ matchingRoom, found ] = Object.entries(connections)
                 .reduce(([room, isFound], [roomKey, roomValue]) => {
-
                     if(!isFound && roomValue.includes(socket.id)) {
                         return [roomKey, true];
                     }
-
                     return [room, isFound];
-
                 }, ['', false]);
 
             if (found === true) {
-
                 if(messages[matchingRoom] === undefined) {
                     messages[matchingRoom] = [];
                 }
-
                 messages[matchingRoom].push({
                     'data': data,
                     'sender': sender,
                     'socket-id-sender': socket.id
                 })
-
                 console.log("messages", key, ":", sender, data)
-
                 connections[matchingRoom].forEach((elem) => {
                     io.to(elem).emit("chat_message", data, sender, socket.id);
                 });
-
             }    
 
         });
-
-        socket.on("disconnected", () => {
-
+        
+        // When a user disconnects, we need to remove the user from the room and notify other users in the room.
+        socket.on("disconnect", () => {
+            const diffTime = Math.abs(timeOnLine[socket.id] - new Date());
+            let key;
+            for (const [k, v] of JSON.parse(JSON.stringify(Object.entries(connections)))) {
+                for (let a=0; a<v.length; a++) {
+                    if(v[a] === socket.id) {
+                        key = k;
+                        for (let b=0; b<connections[key].length; b++) {
+                            io.to(connections[key][b]).emit("user-left", socket.id);
+                        }
+                        const index = connections[key].indexOf(socket.id);
+                        connections[key].splice(index, 1);
+                        if(connections[key].length === 0) {
+                            delete connections[key];
+                        }
+                    }
+                }
+            }
         });
 
 
-
+    });
 
 
 
