a0ae573a72f8f813f8c3d3983f23f0ca10238a08
jayesh-132005-gif <dev.jayeshsingh@gmail.com>
2026-09-13 12:34:24 +0530

    Modified landing.jsx

--- fronted/src/pages/landing.jsx
+++ fronted/src/pages/landing.jsx
@@ -4,11 +4,29 @@ import React from "react";
 export default function Landing() {
     return (
         <div className="landingPageContainer">
+
             <nav>
                 <div className="navHeader">
-                    <h2>ZoomWebClone</h2>
+                    <h2>Nexus Hub</h2>
+                </div>
+                <div className="navList">
+                    <p>Join as a Guest</p>
+                    <p>Register</p>
+                    <div role="button">
+                        Login
+                    </div>
                 </div>
             </nav>
+
+            <div className="landingMainContainer">
+                <div>
+                    <h1><span style={{color: "#ff9839" }}>Connect</span> with your Loved one</h1>
+                    <p>Cover a distance by <span style={{color: "#ff9839" }}>Nexus Hub</span></p>
+                </div>
+                <div>
+                    <img src="/callingImage.png" alt="callingImage" height = "300rem"/>
+                </div>
+            </div>
         </div>
     );
 }
